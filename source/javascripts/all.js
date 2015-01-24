91809fe8d20e7b72fbd0684174edd6fb5b2bf290
bergren2 <dan@heymrbass.com>
2015-01-23 20:30:47 -0600

    bind blocks to grid

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,15 @@
 //= require_tree .
 //= require jquery
 
-var w = 50, // blocks across
-    h = 50, // blocks down
-    p = 10; // block width in pixels
+// global vars
+var P = 10; // block width in pixels
 
+// floors the number based on the block sizes in the grid
+function blockFloor (num) {
+  return num - (num % P);
+}
+
+// returns block coordinates of the click
 function coordsOf (e, $el) {
   var offset = $el.offset();
   var x = e.clientX - offset.left;
@@ -12,6 +17,7 @@ function coordsOf (e, $el) {
   return [x, y];
 }
 
+// creates a road element in the game
 function createRoad ($game, coords) {
   var direction = Math.floor(Math.random() * 2);
   if (direction === 0) {
@@ -20,17 +26,18 @@ function createRoad ($game, coords) {
     console.log('vertical');
   }
 
-  var $road = $('<div></div>').addClass('road').width(p).height(p)
+  var $road = $('<div></div>').addClass('road').width(P).height(P)
                               .css('left', coords[0]).css('top', coords[1]);
   $game.append($road);
 }
 
+// load the game
 $(document).ready(function () {
   var w = $(document).width();
   var h = $(document).height();
 
-  w = w - (w % p);
-  h = h - (h % p);
+  w = blockFloor(w);
+  h = blockFloor(h);
   var $game = $('#game');
   $game.width(w).height(h);
 
